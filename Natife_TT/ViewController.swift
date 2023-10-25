03269c9ae6985eea8b55078f8c367be0f9c36596
strahovochka <103863168+strahovochka@users.noreply.github.com>
2023-10-25 14:14:49 +0300

    start

---
+++ Natife_TT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Natife_TT
+//
+//  Created by Jane Strashok on 25.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+}
+
